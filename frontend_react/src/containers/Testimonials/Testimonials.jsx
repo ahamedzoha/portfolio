acb5de3ec30b9a0e6bcbe1b65f32d2826d6ec118
ahamedzoha <ahamed.zoha@gmail.com>
2022-03-06 21:36:12 +0600

    added Motion Wrapper HOC ++ working on Testimonials

--- frontend_react/src/containers/Testimonials/Testimonials.jsx
+++ frontend_react/src/containers/Testimonials/Testimonials.jsx
@@ -1,8 +1,17 @@
-import React from "react"
+import { useState, useEffect } from "react"
 import "./Testimonials.scss"
+import { motion } from "framer-motion"
+import { AppWrap, MotionWrap } from "../../wrapper"
+import { urlFor, client } from "../../client"
+import { HiChevronRight, HiChevronLeft } from "react-icons/hi"
 
 const Testimonials = () => {
   return <div>Testimonials</div>
 }
 
-export default Testimonials
+// export default Testimonials
+export default AppWrap(
+  MotionWrap(Testimonials, "app__testimonials"),
+  "testimonials",
+  "app__whitebg"
+)

6727238731de774cae45960c5e367a00cfbb9552
ahamedzoha <ahamed.zoha@gmail.com>
2022-03-06 22:54:49 +0600

    updated testimonials section

--- frontend_react/src/containers/Testimonials/Testimonials.jsx
+++ frontend_react/src/containers/Testimonials/Testimonials.jsx
@@ -10,6 +10,10 @@ const Testimonials = () => {
   const [recommendations, setRecommendations] = useState([])
   const [currentIndex, setCurrentIndex] = useState(0)
 
+  const handleClick = (index) => {
+    setCurrentIndex(index)
+  }
+
   const fetchCompanies = async () => {
     const query = `*[_type == "companies"]`
 
@@ -31,12 +35,76 @@ const Testimonials = () => {
       .catch((err) => console.log(err))
   }
 
-  return <div>Testimonials</div>
+  useEffect(() => {
+    fetchCompanies()
+    fetchRecommendations()
+  }, [])
+
+  const recc = recommendations[currentIndex]
+
+  return (
+    <>
+      {recommendations.length && (
+        <>
+          <div className="app__testimonials-item app__flex">
+            <img src={urlFor(recc.imageurl)} alt="recommendation" />
+            <div className="app__testimonials-content">
+              <p className="p-text">{recc.feedback}</p>
+
+              <div className="">
+                <h4 className="bold-text">{recc.name}</h4>
+                <h5 className="p-text">{recc.company}</h5>
+              </div>
+            </div>
+          </div>
+
+          <div className="app__testimonials-btns app__flex">
+            <div
+              className="app__flex"
+              onClick={() =>
+                handleClick(
+                  currentIndex === 0
+                    ? recommendations.length - 1
+                    : currentIndex - 1
+                )
+              }
+            >
+              <HiChevronLeft />
+            </div>
+            <div
+              className="app__flex"
+              onClick={() =>
+                handleClick(
+                  currentIndex === recommendations.length - 1
+                    ? 0
+                    : currentIndex + 1
+                )
+              }
+            >
+              <HiChevronRight />
+            </div>
+          </div>
+        </>
+      )}
+
+      <div className="app__testimonials-companies app__flex">
+        {companies?.map((company, index) => (
+          <motion.div
+            key={company + index}
+            whileInView={{ opacity: [0, 1] }}
+            transition={{ duration: 0.5, type: "tween" }}
+          >
+            <img src={urlFor(company.imgUrl)} alt={company.name} srcset="" />
+          </motion.div>
+        ))}
+      </div>
+    </>
+  )
 }
 
 // export default Testimonials
 export default AppWrap(
   MotionWrap(Testimonials, "app__testimonials"),
   "testimonials",
-  "app__whitebg"
+  "app__primarybg"
 )

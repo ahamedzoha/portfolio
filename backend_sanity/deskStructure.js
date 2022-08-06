d864a8a948b3f1a7c4b2302fdee72518f3d35917
azaz ahamed <azazahamed@azazs-MacBook-Pro.local>
2022-08-07 02:35:07 +0600

    cleanup for a portfolio rewrite

--- backend_sanity/deskStructure.js
+++
@@ -1,7 +0,0 @@
-import S from "@sanity/desk-tool/structure-builder"
-import postsByTag from "./postsByTag"
-
-export default () =>
-  S.list()
-    .title("Base")
-    .items([postsByTag, ...S.documentTypeListItems()])
